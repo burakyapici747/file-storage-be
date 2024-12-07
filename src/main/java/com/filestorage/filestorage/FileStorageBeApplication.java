0d59516bbe02a863281d8240ed9c123ea88adcd1
burakyapici <burakyapici747@gmail.com>
2024-12-07 15:33:30 +0300

    project init

---
+++ src/main/java/com/filestorage/filestorage/FileStorageBeApplication.java
@@ -0,0 +1,13 @@
+package com.filestorage.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filestorage/filestorage/FileStorageBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filestorage.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
